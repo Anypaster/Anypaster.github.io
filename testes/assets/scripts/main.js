eb6ee65981baf2fe41554237956161fe464496a4
Redmixes <Maxiaero228@example.com>
2020-03-23 17:05:37 +0200

    testes bg js update

--- testes/assets/scripts/main.js
+++ testes/assets/scripts/main.js
@@ -1,15 +1,43 @@
-let login = prompt("Enter login");
-if (login == "admin") {
-  let password = prompt("Enter password");
-  if (password == "admin") {
-    alert("Welcome !");
-  } else if (password == null) {
+/////////////////LOGIN/////////////////////////////
+function login() {
+  let login = prompt("Enter login");
+  if (login == "admin") {
+    let password = prompt("Enter password");
+    if (password == "admin") {
+      alert("Welcome !");
+    } else if (password == null) {
+      alert("cancel");
+    } else {
+      alert("incorected password");
+    }
+  } else if (login == null) {
     alert("cancel");
   } else {
-    alert("incorected password");
+    alert("login not found");
   }
-} else if (login == null) {
-  alert("cancel");
-} else {
-  alert("login not found");
 }
+/////////////////////////RECURSION///////////////////////////
+function recursion() {
+  let list = {
+    name: "andry",
+    obg: {
+      lalka: "piisss"
+    }
+  };
+
+  let cloneList = {};
+
+  cloneObg(list, cloneList);
+  console.log(cloneList);
+
+  function cloneObg(original, clone) {
+    for (let prop in original) {
+      if (typeof original[prop] == "object" && original[prop] != null) {
+        clone[prop] = {};
+        cloneObg(original[prop], clone[prop]);
+      }
+      clone[prop] = original[prop];
+    }
+  }
+}
+////////////////////////////////////////////////////////////////
