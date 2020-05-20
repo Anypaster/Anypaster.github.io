78b67d833a14c31e777d70029bd2ca47d821ed13
Redmixes <Maxiaero228@example.com>
2020-05-20 14:29:22 +0300

    Антон 2

--- skachatvirus/hack.js
+++ skachatvirus/hack.js
@@ -1,8 +1,13 @@
-let i = 1;
-for (i; i!=100000; i = i + i){
-    while ( i <= 5000){
-        if (i = 80000) {
-            i = 0;
+function hack() {
+    alert("Your mac was hacked");
+    let i = 1;
+    for (i; i!=100000; ++i){
+        while ( i <= 5000){
+            console.log(i);
+            if (i = 80000) {
+                i = 0;
+            }
         }
     }
 }
+setTimeout(hack, 5000);
