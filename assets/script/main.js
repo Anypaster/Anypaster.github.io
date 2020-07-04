fe1dff0d77b02ef42c387bba071976a13c30000c
Redmixes <Maxiaero228@example.com>
2020-07-04 14:22:27 +0300

    menu#2.1

--- assets/script/main.js
+++ assets/script/main.js
@@ -2,6 +2,12 @@ let setting = document.querySelector('.menu__setting');
 let setting_2 =document.querySelector('.menu__setting-2')
 let setting_menu = document.querySelector(".setting-menu");
 let setting_closed = document.querySelector('.setting-menu__closed');
+let header__arrow_bottom = document.querySelector('.header__arrow-bottom');
+let menu_header__body = document.querySelector('.menu-header__body')
+
+header__arrow_bottom.addEventListener("click", function(){
+    menu_header__body.classList.add("list-active");
+});
 
 setting.addEventListener("click", function(){
     setting_menu.classList.add("_setting-active");
