31cd56a22101d756471ff65ac74c1a1d6b493887
Redmixes <Maxiaero228@example.com>
2020-07-08 10:49:36 +0300

    menu#2.4

--- items/html&css/assets/script/main.js
+++ items/html&css/assets/script/main.js
@@ -2,17 +2,16 @@
 /*                    Setting - Modal Window (pop-up)                    */
 /*///////////////////////////////////////////////////////////////////////*/
 
-const setting = document.querySelector('.menu__setting');
+const setting_btn = document.querySelector('.menu__setting');
 const setting_menu = document.querySelector(".setting-menu");
 const setting_closed = document.querySelector('.setting-menu__closed');
 
-setting.addEventListener("click", function(){
+setting_btn.addEventListener("click", function(){
     setting_menu.classList.add("_setting-active");
 });
 setting_closed.addEventListener("click", function(){
     setting_menu.classList.remove("_setting-active");
 });
-
 /*///////////////////////////////////////////////////////////////////////////*/
 /*                    Mobile Menu - Modal Window (pop-up)                    */
 /*///////////////////////////////////////////////////////////////////////////*/
@@ -26,3 +25,16 @@ mobile_button.addEventListener("click", function(){
     mobile_menu.classList.add("_menu-header__list-open");
     mobile_button.classList.add('_menu-header-deactivate');
 });
+
+document.documentElement.addEventListener("click", function (e) {
+    if (e.target.closest('._text-decoration')){   
+        mobile_menu.classList.remove("_menu-header__list-open");
+        mobile_button.classList.remove('_menu-header-deactivate');
+    }   
+    else { 
+        if(!e.target.closest('.menu-header')) {
+        mobile_menu.classList.remove("_menu-header__list-open");
+        mobile_button.classList.remove('_menu-header-deactivate');
+        }
+    }
+})
